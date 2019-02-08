1644f8ea00cc5e3311f8c64b45ce3c439ba9e2b9
Ural Davletshin <u.davletshin@cognitive.ru>
2019-02-08 16:15:03 +0300

    Add GraphQL libs

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,8 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Overblog\GraphQLBundle\OverblogGraphQLBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Overblog\GraphiQLBundle\OverblogGraphiQLBundle::class => ['dev' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
